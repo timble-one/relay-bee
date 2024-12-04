773ef3823bffcc387563daac8b624a55f0a5d5f4
Timo Bühlmann <timo.buehlmann@gmail.com>
2024-12-05 00:20:38 +0100

    make LoginForm.tsx and PromptLayout.tsx more generic

--- lib/components/LoginForm.tsx
+++ lib/components/LoginForm.tsx
@@ -32,12 +32,13 @@ export function LoginForm({onSuccess}: {onSuccess: () => void}) {
         <Form className="space-y-4 md:space-y-6" onSubmit={loginAction}>
             <div>
                 <label htmlFor="email"
-                       className="block mb-2 text-sm font-medium text-gray-900">
+                       className="block mb-2 text-sm font-medium text-gray-900"
+                >
                     Email
                 </label>
                 <input
                     type="email" name="email" id="email" required={true}
-                    placeholder="name@openairdeisswil.ch"
+                    placeholder="name@relay-bee.com"
                     value={state.email} onChange={(e) => setState({...state, email: e.target.value})}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                 />
--- lib/components/PromptLayout.tsx
+++ lib/components/PromptLayout.tsx
@@ -1,12 +1,16 @@
 import {ReactNode} from "react";
 import {Link} from "found";
 
-type Props = {children: ReactNode, outsideReactRouter?: boolean};
+type Props = {
+    children: ReactNode,
+    outsideReactRouter?: boolean
+    logo?: ReactNode
+}
 
-export function PromptLayout({children, outsideReactRouter}: Props) {
+export function PromptLayout({children, outsideReactRouter, logo}: Props) {
     return <section className="bg-gray-50 h-screen">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <LogoLink outsideReactRouter={outsideReactRouter} />
+            <LogoLink outsideReactRouter={outsideReactRouter} logo={logo} />
             <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     {children}
@@ -16,19 +20,10 @@ export function PromptLayout({children, outsideReactRouter}: Props) {
     </section>
 }
 
-function LogoLink({outsideReactRouter}: {outsideReactRouter?: boolean}) {
+function LogoLink({outsideReactRouter, logo}: {outsideReactRouter?: boolean, logo?: ReactNode}) {
     const linkClasses = 'flex gap-4 items-center mb-6 text-2xl font-semibold text-gray-900';
     if (outsideReactRouter) {
-        return <a href="#" className={linkClasses}><Logo/></a>
+        return <a href="#" className={linkClasses}>{logo}</a>
     }
-    return <Link to="#" className={linkClasses}><Logo/></Link>
-}
-
-function Logo() {
-    return (
-        <>
-            <img src="/oad.svg" alt="logo" className="w-24 mr-2"/>
-            <span className={"text-5xl"}>Admin</span>
-        </>
-    )
+    return <Link to="#" className={linkClasses}>{logo}</Link>
 }
