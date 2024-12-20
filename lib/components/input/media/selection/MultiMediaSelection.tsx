8a00990c60a2e8eff3999798f7bc3d4adb8bfa91
Timo Bühlmann <timo.buehlmann@gmail.com>
2024-12-20 13:32:45 +0100

    tweak MultiMediaSelection.tsx  - finish sorting implementation  - add delete-functionality

--- lib/components/input/media/selection/MultiMediaSelection.tsx
+++ lib/components/input/media/selection/MultiMediaSelection.tsx
@@ -23,6 +23,7 @@ import {
 import {CSS} from '@dnd-kit/utilities';
 import TooltipIcon from "../../../icon/TooltipIcon.tsx";
 import {nameToId, notEmpty, useBackendPath} from "../../../../util/util.ts";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 
 export type SortedMediaObjectCursorConnection = {
     edges: ReadonlyArray<{
@@ -88,11 +89,16 @@ export function MultiMediaSelection<
             const newIndex = overMedia && over !== null ? sortableMedias?.indexOf(overMedia) : undefined
             if (oldIndex !== undefined && newIndex !== undefined && sortableMedias) {
                 onChange({edges:
-                    arrayMove(sortableMedias, oldIndex, newIndex).map(m => ({node: m}))
+                    arrayMove(sortableMedias, oldIndex, newIndex)
+                        .map((m, i) => ({node: {...m, sortingImportance: sortableMedias.length - i}}))
                 })
             }
         }
     };
+
+    const getRemoveHandler = (id: string) => () =>
+        onChange({edges: value?.edges?.filter(e => e?.node?.id !== id)})
+    ;
 
     return (
         <>
@@ -112,7 +118,9 @@ export function MultiMediaSelection<
                         <div className="flex flex-wrap">
                             {sortableMedias &&
                                 <SortableContext items={sortableMedias}>
-                                    {sortableMedias.map(o => <SortableMedia key={o.id} sortedMedia={o} />)}
+                                    {sortableMedias.map(o =>
+                                        <SortableMedia key={o.id} sortedMedia={o} onRemove={getRemoveHandler(o.id)} />
+                                    )}
                                 </SortableContext>
                             }
                         </div>
@@ -138,8 +146,12 @@ export function MultiMediaSelection<
 }
 
 
+type SortableMediaProps = {
+    sortedMedia: SortedMediaObject
+    onRemove: () => void
+}
 
-export function SortableMedia({sortedMedia}: {sortedMedia: SortedMediaObject}) {
+export function SortableMedia({sortedMedia, onRemove}: SortableMediaProps) {
     const backendPath = useBackendPath()
     const contentUrl = sortedMedia.mediaObject.contentUrl
 
@@ -157,11 +169,31 @@ export function SortableMedia({sortedMedia}: {sortedMedia: SortedMediaObject}) {
     }
 
     return (
-        <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            {contentUrl && <img
-                src={backendPath(contentUrl)} alt="image"
-                className="max-h-32 max-w-32"
-            />}
+        <div ref={setNodeRef} style={style} {...attributes}>
+            {contentUrl &&
+                <div className="relative">
+                    <button {...listeners}
+                      type="button"
+                      className="absolute -top-2 right-10 rounded-full bg-indigo-600 p-1 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        <svg className="size-5" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fillRule="evenodd" clipRule="evenodd" fill="#FFFFFF"
+                                d="M9.5 8C10.3284 8 11 7.32843 11 6.5C11 5.67157 10.3284 5 9.5 5C8.67157 5 8 5.67157 8 6.5C8 7.32843 8.67157 8 9.5 8ZM9.5 14C10.3284 14 11 13.3284 11 12.5C11 11.6716 10.3284 11 9.5 11C8.67157 11 8 11.6716 8 12.5C8 13.3284 8.67157 14 9.5 14ZM11 18.5C11 19.3284 10.3284 20 9.5 20C8.67157 20 8 19.3284 8 18.5C8 17.6716 8.67157 17 9.5 17C10.3284 17 11 17.6716 11 18.5ZM15.5 8C16.3284 8 17 7.32843 17 6.5C17 5.67157 16.3284 5 15.5 5C14.6716 5 14 5.67157 14 6.5C14 7.32843 14.6716 8 15.5 8ZM17 12.5C17 13.3284 16.3284 14 15.5 14C14.6716 14 14 13.3284 14 12.5C14 11.6716 14.6716 11 15.5 11C16.3284 11 17 11.6716 17 12.5ZM15.5 20C16.3284 20 17 19.3284 17 18.5C17 17.6716 16.3284 17 15.5 17C14.6716 17 14 17.6716 14 18.5C14 19.3284 14.6716 20 15.5 20Z"
+                            />
+                        </svg>
+                    </button>
+                    <button
+                        type="button" onClick={onRemove}
+                        className="absolute -top-2 right-2 rounded-full bg-red-600 p-1 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                    >
+                        <XMarkIcon aria-hidden="true" className="size-5"/>
+                    </button>
+                    <img
+                        src={backendPath(contentUrl)} alt=""
+                        className="w-48 m-4 aspect-[3/2] rounded-2xl object-cover"
+                    />
+                </div>
+            }
         </div>
     )
 }
