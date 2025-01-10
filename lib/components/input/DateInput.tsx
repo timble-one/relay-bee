bc66cc6809aed48c10246d29f936fe4e3acf9978
Timo Bühlmann <timo.buehlmann@gmail.com>
2025-01-10 17:16:01 +0100

    fix description-hint on date-input

--- lib/components/input/DateInput.tsx
+++ lib/components/input/DateInput.tsx
@@ -1,5 +1,6 @@
 import {nameToId} from "../../util/util.ts";
 import clsx from "clsx";
+import TooltipIcon from "../icon/TooltipIcon.tsx";
 
 type Props = {
     title: string,
@@ -26,6 +27,11 @@ export function DateInput({title, value, onChange = () => undefined, description
                     onChange={(e) => onChange(e.currentTarget.value)}
                     className={clsx(description ? 'pl-3 pr-10' : 'p-3', 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6')}
                 />
+                {description &&
+                  <TooltipIcon iconClasses="absolute inset-y-0 right-0 flex items-center pr-3">
+                      {description}
+                  </TooltipIcon>
+                }
             </div>
         </div>
     )
