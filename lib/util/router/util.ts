ee139a80dd2b260f9751d94e9cb7f541b8e9cc3b
Timo Bühlmann <timo.buehlmann@gmail.com>
2025-02-15 16:01:18 +0100

    useRouter: improve implementation

--- lib/util/router/util.ts
+++ lib/util/router/util.ts
@@ -1,15 +1,16 @@
 import {useEnv} from "../environment/useEnv.ts";
-import {LocationDescriptor, useRouter as useFoundRouter} from "found";
+import {LocationDescriptor, RouterState, useRouter as useFoundRouter} from "found";
 
 export const useRoute = () => {
     const {adminBasePath} = useEnv()
     return (relativePath: LocationDescriptor) => prependToRoute(adminBasePath, relativePath)
 }
 
-export const useRouter = () => {
+export const useRouter = (): RouterState => {
     const route = useRoute()
     const {match, router} = useFoundRouter()
     return {match, router: {
+        ...router,
         push: (path: LocationDescriptor) => router.push(route(path)),
         replace: (path: LocationDescriptor) => router.replace(route(path)),
         go: (delta: number) => router.go(delta),
