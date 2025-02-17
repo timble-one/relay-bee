6380968a3e1c083bf7453e398a06d552c9a07d6e
Timo Bühlmann <timo.buehlmann@gmail.com>
2025-02-17 23:49:36 +0100

    tmp

--- lib/components/Link.tsx
+++ lib/components/Link.tsx
@@ -1,25 +1,13 @@
 import {useRoute} from "../util/router/util.ts";
-import {Link as FoundLink, LinkInjectedProps, LinkProps, LocationDescriptor} from "found"
-import * as React from "react";
+import {Link as FoundLink, LocationDescriptor} from "found"
+import {ReactNode} from "react";
 
-type Props<
-    TInner extends React.ElementType = never,
-    TInnerWithActivePropName extends React.ComponentType<
-        LinkInjectedProps & { [activePropName in TActivePropName]: boolean }
-    > = never,
-    TActivePropName extends string = never,
-> = {children: LinkProps<TInner, TInnerWithActivePropName, TActivePropName>['children'], to: LocationDescriptor, className?: string}
+type Props = {children: ReactNode, to: LocationDescriptor, className?: string}
 
-export const Link = <
-    TInner extends React.ElementType = never,
-    TInnerWithActivePropName extends React.ComponentType<
-        LinkInjectedProps & { [activePropName in TActivePropName]: boolean }
-    > = never,
-    TActivePropName extends string = never,
->({children, to, className}: Props<TInner, TInnerWithActivePropName, TActivePropName>) => {
+export const Link = ({children, to, className}: Props) => {
     const route = useRoute()
     return (
-        <FoundLink<TInner, TInnerWithActivePropName, TActivePropName> to={route(to)} className={className}>
+        <FoundLink to={route(to)} className={className}>
             {children}
         </FoundLink>
     )
