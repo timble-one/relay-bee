b59cc535d26519f9ae2653f9466e6b7bacf74168
Timo Bühlmann <info@timble.one>
2025-08-12 10:35:23 +0200

    DateTimeInput: tweak tailwind-classes

--- lib/components/input/DateTimeInput.tsx
+++ lib/components/input/DateTimeInput.tsx
@@ -38,7 +38,12 @@ export function DateTimeInput(
                     type={type}
                     value={dateWithoutTimezone ?? ''}
                     onChange={(e) => onChange(e.currentTarget.value)}
-                    className={clsx(description ? 'pl-3 pr-10' : 'p-3', 'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6')}
+                    className={clsx(
+                        'block w-full py-1.5', description ? 'pl-3 pr-10' : 'p-3',
+                        'rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300',
+                        'focus:ring-2 focus:ring-inset focus:ring-indigo-600',
+                        'text-gray-900 sm:text-sm sm:leading-6 placeholder:text-gray-400',
+                    )}
                 />
                 {description &&
                   <TooltipIcon iconClasses="absolute inset-y-0 right-0 flex items-center pr-3">
