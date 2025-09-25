abb4bd0800acc59fbadc1cf047cfb774ab6d092f
Timo Bühlmann <hello@timble.one>
2025-09-25 23:04:59 +0200

    v0.7.3: link (don't modify absolute links)

--- lib/components/Link.tsx
+++ lib/components/Link.tsx
@@ -1,16 +1,18 @@
 import {Link as FoundLink, LocationDescriptor} from "found";
 import {ReactNode} from "react";
 import {useRoute} from "../util/router/util.ts";
+import {apply} from "tssentials";
 
 export const Link = (props: {
     to: LocationDescriptor,
     children: ReactNode,
     target?: string,
-    className?: string
+    className?: string,
 }) => {
-    const {className, children} = props
+    const {className, children, to} = props
     const route = useRoute()
-    const url = route(props.to)
+    const url = apply(to.toString(), u =>
+        u.startsWith('http') ? u : route(to))
     return props.target === '_blank'
         ?
             <a
