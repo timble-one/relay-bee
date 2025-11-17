ca0c91c838b3b6d6bcb8a08f64fa88fedf46cae3
Timo Bühlmann <info@timble.one>
2025-11-17 12:59:30 +0100

    number-input: cleanup

--- lib/components/input/NumberInput.tsx
+++ lib/components/input/NumberInput.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import TooltipIcon from "../icon/TooltipIcon.tsx";
 import {nameToId} from "../../util/util.ts";
-import clsx from "clsx";
 import {InputLabel} from "./InputLabel.tsx";
+import {twMerge} from "tailwind-merge";
+import {ifPresent} from "tssentials";
 
 type Props = {
     title: string,
@@ -14,30 +15,33 @@ type Props = {
     min?: number,
     step?: number,
     unit?: string,
-};
+}
 
-export function NumberInput(
-    {title, value, onChange = () => undefined, description, required, min, step, unit}: Props) {
+export function NumberInput(props: Props) {
+    const {title, description} = props
     const onStringChange = (e: React.FormEvent<HTMLInputElement>) => {
-        onChange(Number(e.currentTarget.value));
+        ifPresent(props.onChange, c => c(Number(e.currentTarget.value)))
     }
-    const inputId = nameToId(title);
+    const inputId = nameToId(title)
     return (
         <div className="flex flex-col gap-2 col-span-6 xl:col-span-3 2xl:col-span-2">
-            <InputLabel inputId={inputId} description={description}>{title}</InputLabel>
+            <InputLabel inputId={inputId}>{title}</InputLabel>
             <div className="flex gap-2 items-center">
                 <div className="relative rounded-md shadow-sm grow">
                     <input
                         id={inputId}
                         type="number"
-                        min={min}
-                        step={step}
-                        value={value ?? ''}
+                        min={props.min}
+                        step={props.step}
+                        value={props.value ?? ''}
                         onChange={onStringChange}
-                        required={required}
-                        className={clsx(
-                            'block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-                            description ? 'pl-3 pr-10' : 'p-3',
+                        required={props.required}
+                        className={twMerge(
+                            'block w-full py-1.5',
+                            description && 'pl-3 pr-10',
+                            'shadow-sm rounded-md border-0',
+                            'ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600',
+                            'text-gray-900 sm:text-sm sm:leading-6 placeholder:text-gray-400',
                         )}
                     />
                     {description &&
@@ -46,7 +50,8 @@ export function NumberInput(
                       </TooltipIcon>
                     }
                 </div>
-                {unit && <label className="text-sm">{unit}</label>}
+                {ifPresent(props.unit, u =>
+                    <label className="text-sm">{u}</label>)}
             </div>
         </div>
     )
